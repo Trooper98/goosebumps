4657be1a2b396ed5a35a1da34e5611ec1cb53d1b
Trooper98 <oliver.g.ryan@gmail.com>
2019-01-02 17:34:50 +0100

    main.py

--- src/led.py
+++ src/led.py
@@ -13,32 +13,28 @@ greenPin = 32  # GPIO 12
 switch_On = True
 switch_Off = False
 
+   def __init__(self):
+        setup_pins()
+        # blink lights
+        self.toggle_leds(switch_On)
+        time.sleep(3)
+        self.toggle_leds(switch_Off)
 
-def __init__(self):
-    setup_pins()
-    # blink lights
-    self.toggle_pins(switch_On)
-    time.sleep(3)
-    self.toggle_pins(switch_Off)
+    def setup_pins():
+        # setup the pins accrding to B+ board rather than BCM
+        GPIO.setmode(GPIO.BOARD)
+        # setup the pins
+        GPIO.setup(redPin, GPIO.OUT)
+        GPIO.setup(bluePin, GPIO.OUT)
+        GPIO.setup(yellowPin, GPIO.OUT)
+        GPIO.setup(greenPin, GPIO.OUT)
 
+    def toggle_leds(switch):
+        # light up the pins
+        GPIO.output(redPin, switch)
+        GPIO.output(bluePin, switch)
+        GPIO.output(yellowPin, switch)
+        GPIO.output(greenPin, switch)
 
-def setup_pins():
-    # setup the pins accrding to B+ board rather than BCM
-    GPIO.setmode(GPIO.BOARD)
-    # setup the pins
-    GPIO.setup(redPin, GPIO.OUT)
-    GPIO.setup(bluePin, GPIO.OUT)
-    GPIO.setup(yellowPin, GPIO.OUT)
-    GPIO.setup(greenPin, GPIO.OUT)
-
-
-def toggle_leds(switch):
-    # light up the pins
-    GPIO.output(redPin, switch)
-    GPIO.output(bluePin, switch)
-    GPIO.output(yellowPin, switch)
-    GPIO.output(greenPin, switch)
-
-
-def toggle(pin, switch):
-    GPIO.output(pin, switch)
+    def toggle(pin, switch):
+        GPIO.output(pin, switch)
--- src/test.py
+++
@@ -1,2 +0,0 @@
-from led import Led
-tester = Led()
