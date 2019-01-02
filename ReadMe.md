1d749559d719a2cb4661018ac30aaca6835a8c43
Trooper98 <oliver.g.ryan@gmail.com>
2019-01-02 21:31:02 +0100

    button files for itterating through colors

--- ReadMe.md
+++ ReadMe.md
@@ -8,21 +8,16 @@ Rain? Wind? Temperature? You can bet that Goosebumps will have your back.
 
 ## Setting up
 
-### without RGB Led
-default pins
-    3.3.v
-    GND
-custom pins
-    red = GPIO 23 [pin 16]
-    blue = GPIO 24 [pin 18]
-    yellow = GPIO 25 [pin 22]
-    green = GPIO 12 [pin 32]
-
 ### with RGB Led
 default pins
     3.3.v
     GND
-custom pins
+rgb Led pins
     [x] red = GPIO 17 [pin 11]
     [x] green = GPIO 27 [pin 13]
     [x] blue = GPIO 22 [pin 15]
+
+buttons
+    [button1] = GPIO 23 [pin 16]
+    [button2] = GPIO 24 [pin 18]
+
--- src/buttons.py
+++ src/buttons.py
@@ -8,6 +8,8 @@ import time
 redPin = 11  # GPIO 17
 greenPin = 13  # GPIO 22
 bluePin = 15  # GPIO 27
+button1 = 16  # GPIO 23
+button2 = 18  # GPIO 24
 switch_On = True
 switch_Off = False
 
@@ -49,11 +51,13 @@ def rainbow():
 GPIO.setwarnings(False)
 # setup the pins accrding to B+ board rather than BCM
 GPIO.setmode(GPIO.BOARD)
-# set up main pins
+# set up RGB led pins
 GPIO.setup(redPin, GPIO.OUT)
 GPIO.setup(bluePin, GPIO.OUT)
 GPIO.setup(greenPin, GPIO.OUT)
-
+# set up buttons
+GPIO.setup(button1, GPIO.OUT)
+GPIO.setup(button2, GPIO.OUT)
 
 # setup the pins and switch on the led
 count = 0
@@ -70,3 +74,25 @@ time.sleep(3)
 
 rainbow()
 lightsOut()
+
+
+# get the number of colors
+colorWheel = len(allColors)
+start = 1
+end = colorWheel
+
+current = 1
+while True:
+    setColor(current)
+    if GPIO.input(button1):
+        # get next color
+        if(current == end):  # if the next number is greater than colorwheel
+            current = start
+        else:
+            current += 1
+    else if GPIO.input(button2):
+        # get prev color
+        if(current == start):  # if the next number is greater than colorwheel
+            current = end
+        else:
+            current -= 1
