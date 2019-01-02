394fe6ae80366c109c61a9b86930533e87d9803a
Trooper98 <oliver.g.ryan@gmail.com>
2019-01-02 20:24:48 +0100

    added a rgb led file

--- src/rgb.py
+++ src/rgb.py
@@ -1,22 +1,68 @@
 import RPi.GPIO as GPIO
 import time
-# setup the pins accrding to B+ board rather than BCM
-GPIO.setmode(GPIO.BOARD)
 
 redPin = 11  # GPIO 17
-bluePin = 13  # GPIO 27
-greenPin = 15  # GPIO 22
-_rgb = [redPin, bluePin, greenPin]
+greenPin = 13  # GPIO 22
+bluePin = 15  # GPIO 27
 switch_On = True
 switch_Off = False
 
-# setup the pins and switch on pins
-for pin in _rgb:
-    GPIO.setup(pin, GPIO.OUT)
-    GPIO.output(pin, switch_On)
+# all the color codes
+red = [redPin]
+green = [greenPin]
+blue = [bluePin]
+yellow = [redPin, greenPin]
+puprle = [redPin, bluePin]
+white = [red, blue, green]
+_rgb = [red, green, blue]
+allColors = [red, green, blue, yellow, puprle, white]
 
-time.sleep(10)
+
+def toggle(pin, switch):
+    GPIO.output(pin, switch)
+
+
+def lightsOut():
+    for pin in _rgb:
+        toggle(pin, switch_Off)
+
+
+def setColor(colors):
+    # switch off leds
+    lightsOut()
+    for color in colors:
+        toggle(color, switch_On)
+
+
+# surpress warnings
+GPIO.setwarnings(False)
+# setup the pins accrding to B+ board rather than BCM
+GPIO.setmode(GPIO.BOARD)
+# set up main pins
+GPIO.setup(redPin, GPIO.OUT)
+GPIO.setup(bluePin, GPIO.OUT)
+GPIO.setup(greenPin, GPIO.OUT)
+
+
+# setup the pins and switch on the led
+count = 0
+for pin in _rgb:
+    setColor(pin)
+    if(count < 3):
+        time.sleep(1)
+        count = count + 1
+        print(count)
 
 # switch off leds
-for pin in _rgb:
-    GPIO.output(pin, switch_Off)
+lightsOut()
+time.sleep(3)
+
+for color in allColors:
+    setColor(color)
+    time.sleep(.5)
+
+# setColor(red)
+# time.sleep(5)
+# setColor(green)
+# time.sleep(5)
+# setColor(blue)
