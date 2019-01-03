89ac41208e2017dba3b454c885d510151b276365
Trooper98 <olivermanzi.personal@gmail.com>
2019-01-03 01:37:16 +0100

    hooked up api

--- src/sample/weatherApi.py
+++ src/sample/weatherApi.py
@@ -2,6 +2,7 @@ import config
 import requests
 
 apiKey = config.apiKey
-dataQuery = "https://api.weatherbit.io/v2.0/forecast/hourly?lat=57.705644&lon=11.9368388&key="
-data = requests.get(dataQuery+apiKey)
-print(data.city_name)
+query = "https://api.weatherbit.io/v2.0/forecast/hourly?lat=57.705644&lon=11.9368388&key="
+response = requests.get(query+apiKey)
+data = response.json()
+print(data["city_name"])
