c97142c02472d08b3c2857ddb076cfbec6298a71
Trooper98 <oliver.g.ryan@gmail.com>
2019-01-03 11:50:58 +0100

    refractored button class

--- src/buttons.py
+++ src/buttons.py
@@ -6,22 +6,23 @@ rightB = 16  # GPIO 23 [right]
 
 
 class Button:
-    def __init__(self, button):
-        self.button = button
-        self.buttonPressed = False
+    def __init__(self, pin, name):
+        self.name = name
+        self.pin = pin
+        self.pressed = False
         # surpress warnings
         GPIO.setwarnings(False)
         # setup the pins accrding to B+ board rather than BCM
         GPIO.setmode(GPIO.BOARD)
         # set up buttons
-        GPIO.setup(button, GPIO.IN)
+        GPIO.setup(self.pin, GPIO.IN)
 
     def toggle(self):
         # toggles boolean and returns the new value after toggle
-        self.button = not self.button
-        return self.button
+        self.pressed = not self.pressed
+        return self.pressed
 
 
-b = Button(leftB)
+b = Button(leftB, "left")
 
 print(b.toggle())
--- src/main.py
+++ src/main.py
@@ -4,5 +4,7 @@ from buttons import Button
 import weather_api
 
 data = weather_api.getUpdate()
-leftButton = Button(setup.leftB)
-print(leftButton.toggle())
+leftButton = Button(setup.leftB, "left")
+rightButton = Button(setup.rightB, "right")
+print("{} has a pressed value of {} and a pin number of {}".format(
+    leftButton.name, leftButton.pressed, leftButton.pin))
