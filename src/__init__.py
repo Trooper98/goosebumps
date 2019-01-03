dc4a6760d67960f73f933d8ff8f72421541a7ef6
Trooper98 <oliver.g.ryan@gmail.com>
2019-01-03 14:17:47 +0100

    streamlined project

--- src/__init__.py
+++
@@ -1,6 +0,0 @@
-import config
-import requests
-import sys
-import RPi.GPIO
-import time
-from . import setup
--- src/main.py
+++ src/main.py
@@ -1,11 +1,96 @@
-import rgb
+import RPi.GPIO as GPIO
+import json
+import time
 import setup
-from buttons import Button
-import weather_api
+import weather_api as api
 
-data = weather_api.getUpdate()
-#leftButton = Button(setup.leftB, "left")
-#rightButton = Button(setup.rightB, "right")
-#print("{} has a pressed value of {} and a pin number of {}".format(
-#    leftButton.name, leftButton.pressed, leftButton.pin))
-print(data["city_name"])
+######
+# Button
+leftButton = {
+    "name": "left",
+    "pin": setup.leftB,
+    "switch": False
+}
+
+rightButton = {
+    "name": "right",
+    "pin": setup.rightB,
+    "switch": False
+}
+
+# Led
+led = {
+    "red": setup.redPin,
+    "green": setup.greenPin,
+    "blue": setup.bluePin
+}
+switch_On = True
+switch_Off = False
+#############
+
+
+#############
+# Setting Up
+# surpress warnings
+GPIO.setwarnings(False)
+# setup the pins accrding to B+ board rather than BCM
+GPIO.setmode(GPIO.BOARD)
+
+# set up led #
+GPIO.setup(led["red"], GPIO.OUT)
+GPIO.setup(led["green"], GPIO.OUT)
+GPIO.setup(led["blue"], GPIO.OUT)
+
+# set up buttons #
+GPIO.setup(leftButton["pin"], GPIO.IN)
+GPIO.setup(rightButton["pin"], GPIO.IN)
+#############
+
+
+def toggle(pin, switch):
+    GPIO.output(pin, switch)
+
+
+def lightsOut():
+    for pin in setup._rgb:
+        toggle(pin, switch_Off)
+
+
+def setColor(colors):
+    # switch off leds
+    lightsOut()
+    for color in colors:
+        toggle(color, switch_On)
+
+
+def rainbow(colors):
+    for color in colors:
+        setColor(color)
+        time.sleep(.5)
+
+
+print(leftButton)
+
+start = 0
+end = len(setup.allColors)
+print("end"+str(end))
+
+current = start
+setColor(setup.allColors[current])
+while True:
+    print("current is {}".format(current))
+    if GPIO.input(rightButton["pin"]):
+        print("inside right")
+        nextNum = current + 1
+        if(nextNum == end):
+            current = start
+        else:
+            current += 1
+    elif GPIO.input(leftButton["pin"]):
+        print("inside left")
+        prev = current-1
+        if(prev < start):
+            current = end-1
+        else:
+            current -= 1
+    time.sleep(1)
