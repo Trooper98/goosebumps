727844e2eafb2af2ddf19836a5c792fc4b942ffa
Trooper98 <oliver.g.ryan@gmail.com>
2019-01-12 22:08:10 +0100

    (Patch) meta data was fetching from the wrong source

--- src/components/weather_api.py
+++ src/components/weather_api.py
@@ -37,27 +37,27 @@ class darkSky_api():
 
     def getMetaData(self, rawData):
         data = rawData["hourly"]["data"]
-        coldest = data[0]["temperature"]["temp"]
-        coldestFeel = data[0]["temperature"]["tempFeel"]
-        hottest = data[0]["temperature"]["temp"]
-        hottestFeel = data[0]["temperature"]["tempFeel"]
-        rainChance = data[0]["precip"]["precipProb"]
-        wind = data[0]["wind"]["windSpeed"]
+        coldest = data[0]["temperature"]
+        coldestFeel = data[0]["apparentTemperature"]
+        hottest = data[0]["temperature"]
+        hottestFeel = data[0]["apparentTemperature"]
+        rainChance = data[0]["precipProbability"]
+        wind = data[0]["windSpeed"]
         count = 0
 
         for unit in data:
-            if unit["temperature"]["temp"] < coldest:
-                coldest = unit["temperature"]["temp"]
-            if unit["temperature"]["tempFeel"] < coldestFeel:
+            if unit["temperature"] < coldest:
+                coldest = unit["temperature"]
+            if unit["apparentTemperature"] < coldestFeel:
                 coldestFeel = unit["temperature"]["tempFeel"]
-            if unit["temperature"]["temp"] > hottest:
-                hottest = unit["temperature"]["temp"]
-            if unit["temperature"]["tempFeel"] < hottestFeel:
-                hottestFeel = unit["temperature"]["tempFeel"]
-            if unit["precip"]["precipProb"] > rainChance:
-                rainChance = unit["precip"]["precipProb"]
-            if unit["wind"]["windSpeed"] > wind:
-                wind = unit["wind"]["windSpeed"]
+            if unit["temperature"] > hottest:
+                hottest = unit["temperature"]
+            if unit["apparentTemperature"] < hottestFeel:
+                hottestFeel = unit["apparentTemperature"]
+            if unit["precipProbability"] > rainChance:
+                rainChance = unit["precipProbability"]
+            if unit["windSpeed"] > wind:
+                wind = unit["windSpeed"]
             count += 1
             if(count == 12):
                 break
